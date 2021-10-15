defb91ad2202585c2ea5c4a8ebdb90bd83478427
srkwon <sunryul23@naver.com>
2021-10-15 11:41:02 +0900

    add

---
+++ data-analysis-2021/run-jupyter-notebook.bat
@@ -0,0 +1,16 @@
+:: jupyter lab start batch script(for windows)
+
+:: activate python venv module
+:: if not exists, "python -m venv venv" will work
+call venv\Scripts\activate
+
+:: install modules in requirements.txt using pip
+call pip install -r requirements.txt
+
+:: start jupyter notebook
+call jupyter notebook
+
+
+
+
+
